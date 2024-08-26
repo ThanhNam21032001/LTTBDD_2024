b5c18c4a05ce60cc1e78182617096c5763b97a9a
Thanh Nam <fauxius2132001@gmail.com>
2024-08-26 12:13:02 +0700

    Cau4

---
+++ week1/code4.js
@@ -0,0 +1,11 @@
+const bill = 275;
+const bill1 = 40;
+const bill2 = 430;
+
+const tip = bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.2;
+const tip1 = bill1 <= 300 && bill1 >= 50 ? bill1 * 0.15 : bill1 * 0.2;
+const tip2 = bill2 <= 300 && bill2 >= 50 ? bill2 * 0.15 : bill2 * 0.2;
+
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
+console.log(`The bill was ${bill1}, the tip was ${tip1}, and the total value ${bill1 + tip1}`);
+console.log(`The bill was ${bill2}, the tip was ${tip2}, and the total value ${bill2 + tip2}`);

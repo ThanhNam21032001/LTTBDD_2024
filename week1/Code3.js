4ecdb58f12ee123525c899e98f8ead972d205d90
Thanh Nam <fauxius2132001@gmail.com>
2024-08-26 11:58:03 +0700

    Cau3

---
+++ week1/Code3.js
@@ -0,0 +1,56 @@
+//Data1
+const scoreDolphins = (96 + 108 + 89) / 3;
+const scoreKoalas = (88 + 91 + 110) / 3;
+console.log(scoreDolphins, scoreKoalas);
+
+if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+  console.log("Dolphins win the trophy ");
+} else if (scoreKoalas > scoreDolphins & scoreKoalas >= 100) {
+  console.log("Koalas win the trophy 🏆");
+} else if (
+  scoreDolphins === scoreKoalas &&
+  scoreDolphins >= 100 &&
+  scoreKoalas >= 100
+) {
+  console.log("Both win the trophy!");
+} else {
+  console.log("No one wins the trophy ");
+}
+
+//Bonus1
+const scoreDolphins1 = (97 + 112 + 101) / 3;
+const scoreKoalas1 = (109 + 95 + 123) / 3;
+console.log(scoreDolphins1, scoreKoalas1);
+
+if (scoreDolphins1 > scoreKoalas1 && scoreDolphins1 >= 100) {
+  console.log("Dolphins win the trophy ");
+} else if (scoreKoalas1 > scoreDolphins1 && scoreKoalas1 >= 100) {
+  console.log("Koalas win the trophy ");
+} else if (
+  scoreDolphins1 === scoreKoalas1 &&
+  scoreDolphins1 >= 100 &&
+  scoreKoalas1 >= 100
+) {
+  console.log("Both win the trophy!");
+} else {
+  console.log("No one wins the trophy ");
+}
+
+//Bonus2
+const scoreDolphins2 = (97 + 112 + 101) / 3;
+const scoreKoalas2 = (109 + 95 + 106) / 3;
+console.log(scoreDolphins2, scoreKoalas2);
+
+if (scoreDolphins2 > scoreKoalas2 && scoreDolphins2 >= 100) {
+  console.log("Dolphins win the trophy ");
+} else if (scoreKoalas2 > scoreDolphins2 && scoreKoalas2 >= 100) {
+  console.log("Koalas win the trophy ");
+} else if (
+  scoreDolphins2 === scoreKoalas2 &&
+  scoreDolphins2 >= 100 &&
+  scoreKoalas2 >= 100
+) {
+  console.log("Both win the trophy!");
+} else {
+  console.log("No one wins the trophy ");
+}

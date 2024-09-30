83910e7ef3ac596e58464f4f3fee46092e36b3da
Thanh Nam <fauxius2132001@gmail.com>
2024-09-30 11:50:54 +0700

    Tao page 1

--- week5/App.js
+++ week5/App.js
@@ -1,48 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView,Image, Button } from 'react-native';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native" 
+import { createNativeStackNavigator } from "@react-navigation/native-stack"; 
+import Page1 from "./page/page1";
 
+const Stack = createNativeStackNavigator();
 export default function App() {
+
   return (
-    <SafeAreaView style={styles.container}>
-      <View style={styles.topView}>
-        <Image style={styles.phoneImg} source={require('./Img/vs_blue.png')}/>
-      </View >
-      <View style={styles.middleView}>
-        <Text>Điện thoại VSmart Joy 3- Hàng chính hãng</Text>
-      </View>
-      <View style={styles.bottomView}>
-        <Button style={styles.button} title='Chọn mua'>
-        </Button>
-      </View>
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Page1">
+        <Stack.Screen name="Page1" component={Page1} />
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
+
 }
-
-const styles = StyleSheet.create({
-  button:{
-    backgroundColorcolor:'red'
-  },
-  topView:{
-    flex:2,
-    marginTop:50,
-    alignItems:'center'
-
-  },
-  phoneImg:{
-    height:400,
-  },
-  middleView:{
-    flex:1
-  },
-  bottomViewView:{
-    flex:1
-  },
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignContent:'center',
-    justifyContent:'center',
-    alignItems:'center'
-    
-  },
-});
